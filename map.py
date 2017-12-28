fed2370b4f59daa587285b0f60c4adc94fc0ab07
vitycola <v.valero22@gmail.com>
2017-12-28 19:09:24 +0100

    Preparado el mapper para ejecuccion local

--- map.py
+++ map.py
@@ -1,13 +1,56 @@
+#!/usr/bin/env python
 import json
+import sys
 
 
 
-with open('./datos/sample.txt') as file:
-    for line in file:
-        tweets = json.loads(line) #Por cada fila cargo un JSON
+with open('./datos/sample.txt') as data:
+
+    for line in data:
         try:
-            print tweets['place'] #Imprimo el atributo 'place' dentro del try y except
+            tweets = json.loads(line) #Por cada fila cargo un JSON
+        except: pass
+
+        try:
+
+            if tweets['place']['country'] == 'United States': #Me quedo con los tweets de EEUU
+                words = tweets['text'].split() #Separo en palabras
+                cityTweet = tweets['place']['name'] # me quedo con el nombre de la ciudad
+
+                data2 = open("./files/city.csv") #Cargo el diccionario de ciudades y estados
+                for cities in data2:
+                    city, states = cities.split(",")
+                    city = str(city)
+                    states = str(states)
+
+                    if str(city).lower() == cityTweet.lower():
+                        state = states.split("\n")[0] #Me quito el salto de linea y devuelvo el estado de la ciudad del tweet
+
+
+                analysis = 0
+                count=0
+                happynes = 0
+
+                for word in words: # por cada palabra del tweet
+
+                    file = open("./files/AFINN-111.txt")
+                    for line2 in file: # reviso si la palabra esta en el diccionario
+                        term, score = line2.split("\t")
+                        term = str(term)
+                        score = int(score)
+
+                        try:
+                            if str(word).lower() == term:
+                                analysis = analysis + score
+                                count += 1
+                        except: pass
+                happynes = analysis/count #devuelvo la puntuacion de palabras entre el numero de palabras analizadas
+
+
+                print '%s%s%d' % (str(state),',',happynes) #Mapeo las palabras en formato tupla
+
         except:
-           print("No existen datos")
+            pass
+
 
 
