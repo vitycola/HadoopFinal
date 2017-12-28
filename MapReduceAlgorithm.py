4f6eb6c45709bd090c013966b0ed6de6c69d03c7
vitycola <v.valero22@gmail.com>
2017-12-29 00:31:13 +0100

    Listo

--- MapReduceAlgorithm.py
+++ MapReduceAlgorithm.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import json
 import sys
 
 
@@ -7,35 +6,37 @@ init_state = 'null'
 init_count = 0
 init_score = 0
 
-with open('./prueba_mapper.txt') as data:
+#with open('./prueba_mapper.txt') as data:
 
-    for line in data:
-    #for line in sys.stdin:
-        #try:
-            #item = json.loads(line) #Carga de un JSON
-        #except: pass
+    #for line in data:
+for line in sys.stdin:
 
         #try:
 
-            state, score = line.split(',')
-            state = str(state)
-            score = int(score)
-            print('inicial', state, score)
-            if state == init_state:
-                init_score = init_score + score
-                init_count = init_count + 1
+        state, score = line.split(',')
+        state = str(state)
+        score = int(score)
 
+
+        if state == init_state:
+            #print('inicial', state, score)
+            init_score = init_score + score
+            init_count = init_count + 1
+
+        else:
+            if init_state == 'null':
+                init_state = state
+                init_score = score
+                init_count = 1
             else:
-                if init_state == 'null':
-                    init_state = state
-                    init_score = score
-                    init_count = 1
-                else:
-                    total_score = init_score/init_count
-                    print '%s%s%d' % (str(init_state), ',', total_score)
-                    init_state = state
-                    init_count = 1
-                    init_score = score
+                total_score = round(float(init_score)/float(init_count),3)
+                print '%s%s%s' % (str(init_state), ',', total_score)
+                init_state = state
+                init_count = 1
+                init_score = score
+
+if init_state == state:
+    print '%s%s%s' % (str(init_state), ',', total_score)
 
 
         #except: pass
--- map.py
+++ map.py
@@ -4,53 +4,53 @@ import sys
 
 
 
-with open('./datos/sample.txt') as data:
+#with open('./datos/sample.txt') as data:
+for line in sys.stdin:
+#    for line in data:
+    try:
+        tweets = json.loads(line) #Por cada fila cargo un JSON
+    except: pass
 
-    for line in data:
-        try:
-            tweets = json.loads(line) #Por cada fila cargo un JSON
-        except: pass
+    try:
 
-        try:
+        if tweets['place']['country'] == 'United States': #Me quedo con los tweets de EEUU
+            words = tweets['text'].split() #Separo en palabras
+            cityTweet = tweets['place']['name'] # me quedo con el nombre de la ciudad
 
-            if tweets['place']['country'] == 'United States': #Me quedo con los tweets de EEUU
-                words = tweets['text'].split() #Separo en palabras
-                cityTweet = tweets['place']['name'] # me quedo con el nombre de la ciudad
+            data2 = open("./files/city.csv") #Cargo el diccionario de ciudades y estados
+            for cities in data2:
+                city, states = cities.split(",")
+                city = str(city)
+                states = str(states)
 
-                data2 = open("./files/city.csv") #Cargo el diccionario de ciudades y estados
-                for cities in data2:
-                    city, states = cities.split(",")
-                    city = str(city)
-                    states = str(states)
-
-                    if str(city).lower() == cityTweet.lower():
-                        state = states.split("\n")[0] #Me quito el salto de linea y devuelvo el estado de la ciudad del tweet
-
-
-                analysis = 0
-                count=0
-                happynes = 0
-
-                for word in words: # por cada palabra del tweet
-
-                    file = open("./files/AFINN-111.txt")
-                    for line2 in file: # reviso si la palabra esta en el diccionario
-                        term, score = line2.split("\t")
-                        term = str(term)
-                        score = int(score)
-
-                        try:
-                            if str(word).lower() == term:
-                                analysis = analysis + score
-                                count += 1
-                        except: pass
-                happynes = analysis/count #devuelvo la puntuacion de palabras entre el numero de palabras analizadas
+                if str(city).lower() == cityTweet.lower():
+                    state = states.split("\n")[0] #Me quito el salto de linea y devuelvo el estado de la ciudad del tweet
 
 
-                print '%s%s%d' % (str(state),',',happynes) #Mapeo las palabras en formato tupla
+            analysis = 0
+            count=0
+            happynes = 0
 
-        except:
-            pass
+            for word in words: # por cada palabra del tweet
+
+                file = open("./files/AFINN-111.txt")
+                for line2 in file: # reviso si la palabra esta en el diccionario
+                    term, score = line2.split("\t")
+                    term = str(term)
+                    score = int(score)
+
+                    try:
+                        if str(word).lower() == term:
+                            analysis = analysis + score
+                            count += 1
+                    except: pass
+            happynes = analysis/count #devuelvo la puntuacion de palabras entre el numero de palabras analizadas
+
+
+            print '%s%s%d' % (str(state),',',happynes) #Mapeo las palabras en formato tupla
+
+    except:
+        pass
 
 
 
